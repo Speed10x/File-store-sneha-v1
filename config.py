8daacabf82e6604a48faec344669f5d1321f56fb
Hemant Kumar <160343394+Speed10x@users.noreply.github.com>
2024-12-14 17:23:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,11 +18,11 @@ def is_enabled(value, default):
         return default
       
 # Bot Information
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
+API_ID = int(environ.get("API_ID", "23148998"))
+API_HASH = environ.get("API_HASH", "d948f2f1b811b3fa298cd10f9add3331")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
+PICS = (environ.get('PICS', 'https://graph.org/file/f8b67added3262575819a-089af052a391b2d5bc.jpg')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
 BOT_USERNAME = environ.get("BOT_USERNAME", "") # without @
 PORT = environ.get("PORT", "8080")
