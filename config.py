eb3609eb7f2ac17b4503112cca695818f4ece51f
Hemant Kumar <160343394+Speed10x@users.noreply.github.com>
2024-12-15 13:52:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -61,7 +61,7 @@ VERIFY_MODE = bool(environ.get('VERIFY_MODE', True)) # Set True or False
 # If Verify Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
 SHORTLINK_URL = environ.get("SHORTLINK_URL", "api.shareus.io") # shortlink domain without https://
 SHORTLINK_API = environ.get("SHORTLINK_API", "raXBls27RSYrdSsLIpwfjmR8Bm62") # shortlink api
-VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/How_To_Open_Linkl") # how to open link 
+VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/testingdb2/10") # how to open link 
 
 # Website Info:
 WEBSITE_URL_MODE = bool(environ.get('WEBSITE_URL_MODE', False)) # Set True or False
